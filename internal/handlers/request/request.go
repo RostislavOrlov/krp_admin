9df10180946ae05e48067403197c5c9cff2c1ee9
Rostislav Orlov <orlovrosti@mail.ru>
2024-03-09 22:48:20 +0300

    logrus delete 2

--- internal/handlers/request/request.go
+++ internal/handlers/request/request.go
@@ -8,9 +8,12 @@ import (
 
 func GetRequest[T any](c *gin.Context) (T, bool) {
 	var request T
-
+	//var myMap map[string]interface{}
+	//logrus.Info("запрос (пустой интерфейс): ", c.BindJSON(&myMap))
+	logrus.Info("gin context: ", c)
 	if err := c.BindJSON(&request); err != nil {
 		logrus.Info("запрос: ", request)
+
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return request, false
 	}
